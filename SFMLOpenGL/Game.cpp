fa0b40909c778d10071d6810e9702e177f6eb9b8
(Student) - Michael Rainsford Ryan <c00239510@ITCARLOW.IE>
2020-01-20 11:06:17 +0000

    Added the first cube face

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -15,10 +15,8 @@ void Game::run()
 
 	Event event;
 
-	while (isRunning) {
-
-		cout << "Game running..." << endl;
-
+	while (isRunning) 
+	{
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
@@ -49,34 +47,52 @@ void Game::initialize()
 {
 	isRunning = true;
 
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
+	glTranslatef(0.0f, 0.0f, -10.0f);
+
 	glewInit();
 
 	/* Vertices counter-clockwise winding */
 
-	vertex[0].coordinate[0] = -0.5f;
-	vertex[0].coordinate[1] = -0.5f;
-	vertex[0].coordinate[2] = 0.0f;
+	// Coordinates
+	vertex[0].coordinate[0] = -1.0f;
+	vertex[0].coordinate[1] = -1.0f;
+	vertex[0].coordinate[2] = 1.0f;
 
-	vertex[1].coordinate[0] = -0.5f;
-	vertex[1].coordinate[1] = 0.5f;
-	vertex[1].coordinate[2] = 0.0f;
+	vertex[1].coordinate[0] = 1.0f;
+	vertex[1].coordinate[1] = -1.0f;
+	vertex[1].coordinate[2] = 1.0f;
 
-	vertex[2].coordinate[0] = 0.5f;
-	vertex[2].coordinate[1] = 0.5f;
-	vertex[2].coordinate[2] = 0.0f;
+	vertex[2].coordinate[0] = 1.0f;
+	vertex[2].coordinate[1] = 1.0f;
+	vertex[2].coordinate[2] = 1.0f;
 
-	//vertex[3].coordinate[0] = 0.5f; 
-	//vertex[3].coordinate[1] = 0.5f;  
-	//vertex[3].coordinate[2] = 0.0f;
+	vertex[3].coordinate[0] = -1.0f;
+	vertex[3].coordinate[1] = 1.0f;
+	vertex[3].coordinate[2] = 1.0f;
 
-	//vertex[4].coordinate[0] = 0.5f; 
-	//vertex[4].coordinate[1] = -0.5f;  
-	//vertex[4].coordinate[2] = 0.0f;
+	
+	vertex[4].coordinate[0] = -1.0f;
+	vertex[4].coordinate[1] = -1.0f;
+	vertex[4].coordinate[2] = -1.0f;
 
-	//vertex[5].coordinate[0] = -0.5f; 
-	//vertex[5].coordinate[1] = -0.5f;  
-	//vertex[5].coordinate[2] = 0.0f;
+	vertex[5].coordinate[0] = 1.0f;
+	vertex[5].coordinate[1] = -1.0f;
+	vertex[5].coordinate[2] = -1.0f;
 
+	/*
+	vertex[6].coordinate[0] = 1.0f;
+	vertex[6].coordinate[1] = 1.0f;
+	vertex[6].coordinate[2] = -1.0f;
+
+	vertex[7].coordinate[0] = -1.0f;
+	vertex[7].coordinate[1] = 1.0f;
+	vertex[7].coordinate[2] = -1.0f;*/
+	
+	// Colours
 	vertex[0].color[0] = 0.1f;
 	vertex[0].color[1] = 1.0f;
 	vertex[0].color[2] = 0.0f;
@@ -100,10 +116,43 @@ void Game::initialize()
 	vertex[5].color[0] = 0.6f;
 	vertex[5].color[1] = 1.0f;
 	vertex[5].color[2] = 0.0f;
+	/*
+	vertex[4].color[0] = 0.4f;
+	vertex[4].color[1] = 1.0f;
+	vertex[4].color[2] = 0.0f;
 
+	vertex[5].color[0] = 0.5f;
+	vertex[5].color[1] = 1.0f;
+	vertex[5].color[2] = 0.0f;
+
+	vertex[6].color[0] = 0.6f;
+	vertex[6].color[1] = 1.0f;
+	vertex[6].color[2] = 0.0f;
+
+	vertex[7].color[0] = 0.5f;
+	vertex[7].color[1] = 1.0f;
+	vertex[7].color[2] = 0.0f;*/
 
 	triangles[0] = 0;   triangles[1] = 1;   triangles[2] = 2;
-	triangles[3] = 3;   triangles[4] = 4;   triangles[5] = 5;
+	triangles[3] = 2;   triangles[4] = 3;   triangles[5] = 0;
+
+	/*triangles[0] = 1;   triangles[1] = 5;   triangles[2] = 6;
+	triangles[3] = 6;   triangles[4] = 2;   triangles[5] = 1;
+	triangles[6] = 4;   triangles[7] = 0;   triangles[8] = 3;
+	triangles[9] = 3;   triangles[10] = 7;   triangles[11] = 4;
+	triangles[12] = 3;   triangles[13] = 2;   triangles[14] = 6;
+	triangles[15] = 6;   triangles[16] = 7;   triangles[17] = 3;
+	triangles[18] = 0;   triangles[19] = 4;   triangles[20] = 5;
+	triangles[21] = 5;   triangles[22] = 1;   triangles[23] = 0;
+	triangles[24] = 0;   triangles[25] = 1;   triangles[26] = 2;
+	triangles[27] = 2;   triangles[28] = 3;   triangles[29] = 0;
+	triangles[30] = 4;   triangles[31] = 7;   triangles[32] = 6;
+	triangles[33] = 6;   triangles[34] = 5;   triangles[35] = 4;*/
+
+	/*for (int i = 0; i < 8; i++)
+	{
+		vertex[i].coordinate[2] -= 5;
+	}*/
 
 	/* Create a new VBO using VBO id */
 	glGenBuffers(1, vbo);
@@ -112,7 +161,7 @@ void Game::initialize()
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 
 	/* Upload vertex data to GPU */
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 6, vertex, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 4, vertex, GL_STATIC_DRAW);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 	glGenBuffers(1, &index);
@@ -125,40 +174,16 @@ void Game::update()
 {
 	elapsed = clock.getElapsedTime();
 
-	if (elapsed.asSeconds() >= 1.0f)
+	if (elapsed.asSeconds() >= 1.0f / 60.0f)
 	{
 		clock.restart();
 
-		if (!flip)
-		{
-			flip = true;
-		}
-		else
-			flip = false;
+		
 	}
-
-	if (flip)
-	{
-		rotationAngle += 0.005f;
-
-		if (rotationAngle > 360.0f)
-		{
-			rotationAngle -= 360.0f;
-		}
-	}
-
-	//Change vertex data
-	vertex[0].coordinate[0] += -0.0001f;
-	vertex[0].coordinate[1] += -0.0001f;
-	vertex[0].coordinate[2] += -0.0001f;
-
-	cout << "Update up" << endl;
 }
 
 void Game::render()
 {
-	cout << "Drawing" << endl;
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -174,7 +199,7 @@ void Game::render()
 	glEnableClientState(GL_COLOR_ARRAY);
 
 
-	glColorPointer(3, GL_FLOAT, sizeof(Vertex), (char*)NULL + 12);
+	glColorPointer(3, GL_FLOAT, sizeof(Vertex), (char*)NULL + 0);
 
 	/*	Draw Triangle from VBO	(set where to start from as VBO can contain 
 		model compoents that are and are not to be drawn )	*/
